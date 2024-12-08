b2da322c95f8cf641955a88c6d77b355ccdae669
Marco Zenere <895876@stud.unive.it>
2024-12-08 21:53:19 +0100

    Zenere 08/12/2024 Aggiunta activity_prodotto_singolo.xml con relativa Activity, aggiornamento base grafica, aggiunta bottoni responsabili di visualizzare il prodotto in maniera automatica

--- app/src/main/java/com/example/tooltrip/ToolAdapter.java
+++ app/src/main/java/com/example/tooltrip/ToolAdapter.java
@@ -1,8 +1,10 @@
 package com.example.tooltrip;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
@@ -26,7 +28,16 @@ public class ToolAdapter extends RecyclerView.Adapter<ToolAdapter.ToolViewHolder
         holder.textViewNome.setText(item.getNome());
         holder.textViewDescrizione.setText(item.getDescrizione());
         holder.textViewCategoria.setText(item.getCategoria());
+
+        holder.btnVisualizza.setOnClickListener(v -> {
+            Intent intent = new Intent(holder.itemView.getContext(), VisualizzaProdottoSingoloActivity.class);
+            intent.putExtra("itemNome", item.getNome());
+            intent.putExtra("itemDescrizione", item.getDescrizione());
+            intent.putExtra("itemCategoria", item.getCategoria());
+            holder.itemView.getContext().startActivity(intent);
+        });
     }
+
 
     @Override
     public int getItemCount() {
@@ -35,13 +46,16 @@ public class ToolAdapter extends RecyclerView.Adapter<ToolAdapter.ToolViewHolder
 
     public static class ToolViewHolder extends RecyclerView.ViewHolder {
         TextView textViewNome, textViewDescrizione, textViewCategoria;
+        Button btnVisualizza;
 
         public ToolViewHolder(View itemView) {
             super(itemView);
             textViewNome = itemView.findViewById(R.id.textViewNome);
             textViewDescrizione = itemView.findViewById(R.id.textViewDescrizione);
             textViewCategoria = itemView.findViewById(R.id.textViewCategoria);
+            btnVisualizza = itemView.findViewById(R.id.btnVisualizza);
         }
     }
+
 }
 
